c41799027fca090be6f6ce2856e6decd6a405e6d
timyrapple <wwwtimyrapple228@mail.ru>
2022-02-06 22:48:05 +0500

    commit

--- vsbot.py
+++ vsbot.py
@@ -23,6 +23,7 @@ def bot_message(message):
     if message.chat.type =='private':
         if message.text == "Вход в приват чат":
             bot.send_message(message.chat.id, 'https://qiwi.com/n/SSTAILE' )
+            bot.send_message(message.chat.id, 'Вход в приват чат = 39р (навсегда)' )
         elif message.text == "Отзывы":
            markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
            back = types.KeyboardButton ("Назад")
@@ -36,6 +37,7 @@ def bot_message(message):
             markup.add(back)
 
             bot.send_message(message.chat.id, 'https://qiwi.com/n/SSTAILE', reply_markup =  markup)
+            bot.send_message(message.chat.id, 'Реклама:  1 час - 49 рублей, 3 часа - 69 рублей, 5 часов - 80 рублей, 10 часов - 100 рублей, 12 часов - 159 рублей, ∞ часов - 359 рублей' , reply_markup = markup)
             bot.send_message(message.chat.id, 'После оплаты рекламы, обратитесь к админу, прайс лист на реклманое время указан в группе.', reply_markup = markup)
 
         elif message.text == "Назад":
